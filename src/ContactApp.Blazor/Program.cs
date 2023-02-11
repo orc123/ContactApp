9593bd6bfc9228ae8f218a69b4413b7b6039b166
Phạm Ngọc Đông <dong.orc12@gmail.com>
2023-02-11 23:09:31 +0700

    theme hệ thống

---
+++ src/ContactApp.Blazor/Program.cs
@@ -0,0 +1,39 @@
+using ContactApp.Blazor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor;
+using MudBlazor.Services;
+
+namespace ContactApp.Blazor;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var builder = WebAssemblyHostBuilder.CreateDefault(args);
+        builder.RootComponents.Add<App>("#app");
+        builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddAuthorizationCore();
+
+        builder.Services.AddScoped(sp => new HttpClient
+        {
+            BaseAddress = new Uri(builder.Configuration["BackendApiUrl"])
+        });
+
+
+        builder.Services.AddMudServices(config =>
+        {
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.TopCenter;
+
+            config.SnackbarConfiguration.PreventDuplicates = false;
+            config.SnackbarConfiguration.NewestOnTop = false;
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 10000;
+            config.SnackbarConfiguration.HideTransitionDuration = 500;
+            config.SnackbarConfiguration.ShowTransitionDuration = 500;
+            config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+        });
+
+        await builder.Build().RunAsync();
+    }
+}
---
+++ src/ContactApp.Blazor/Shared/MainLayout.razor.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor.ThemeManager;
+using MudBlazor;
+using ContactApp.Blazor.Theme;
+
+namespace ContactApp.Blazor.Shared;
+
+public partial class MainLayout
+{
+    private ThemeManagerTheme _themeManager = new ThemeManagerTheme();
+    public bool _drawerOpen = true;
+    public bool _themeManagerOpen = false;
+    public string fullName = string.Empty;
+    public long count = 0;
+    int pageIndex = 1;
+    int pageSize = 8;
+    void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+
+    //void OpenThemeManager(bool value)
+    //{
+    //    _themeManagerOpen = value;
+    //}
+
+    //void UpdateTheme(ThemeManagerTheme value)
+    //{
+    //    _themeManager = value;
+    //    StateHasChanged();
+    //}
+
+    protected override async Task OnInitializedAsync()
+    {
+        _themeManager.Theme = new MudBlazorTheme();
+        _themeManager.DrawerClipMode = DrawerClipMode.Always;
+        _themeManager.FontFamily = "Montserrat";
+        _themeManager.DefaultBorderRadius = 3;
+        _themeManager.AppBarElevation = 4;
+        _themeManager.DrawerElevation = 25;
+        StateHasChanged();
+    }
+
+    private async Task HandleLogout()
+    {
+        //await AuthService.LogoutAsync();
+        //NavigationManager.NavigateTo(UrlConstants.Login);
+    }
+}
